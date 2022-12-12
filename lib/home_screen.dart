b8cb621ad6a2463deb2ee761b650f63f956a6fc5
Abdul Awal <abdulawalarif0011@gmail.com>
2022-12-12 17:04:39 +0600

    first object is shown on the screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,9 +12,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<AndroidVerison> androidListWithRandomKeys = [];
-  List<AndroidVerison> androidList = [];
-
   //TODO Working code
   //if data _InternalLinkedHashMap<>..... iterating over the indexes lengths then...
   //TODO first checking from the data set is it _inter.. or Lis<Map<>>..
@@ -43,40 +40,45 @@ class _HomeScreenState extends State<HomeScreen> {
   // print(data[i]['title']);
   // }
   // TODO Working code Second Section
-
-  Future<List<AndroidVerison>> getAndroidVersions(var data) async {
-    //print(data[0].runtimeType.toString());
+  List<AndroidVerison> androidListWithRandomKeys = [];
+  List<AndroidVerison> androidList = [];
+  getAndroidVersions(var data) {
     for (int i = 0; i < data.length; i++) {
       //first type
       if (data[i].runtimeType == List<Map<String, Object>>) {
         var firstData = data[i] as List;
-        print("Data 1:");
+        androidList.clear();
         for (int i = 0; i < firstData.length; i++) {
-          print(firstData[i]['title']);
+          //print(firstData[i]['title']);   androidList
+          AndroidVerison androidVerison = AndroidVerison(
+              id: firstData[i]['id'], title: firstData[i]['title']);
+          androidList.add(androidVerison);
         }
       }
-      //second Type
+      //second Type random keys
       if (data[i].runtimeType != List<Map<String, Object>>) {
-        print("Data 0:");
+        androidListWithRandomKeys.clear();
         final Map<String, dynamic> convertedData =
             jsonDecode(jsonEncode(data[i]));
         var map = HashMap.from(convertedData);
         // //I can fetch the data from here..
-        // //print(map['0']['title']);
         for (int i = 0; i < map.length; i++) {
-          //   //because the pattern is showing the keys as 0/1/3
+          //because the pattern is showing the keys as 0/1/3
           if (i < 2) {
-            print(map['$i']['title']);
+            AndroidVerison androidVerison =
+                AndroidVerison(id: map['$i']['id'], title: map['$i']['title']);
+            androidListWithRandomKeys.add(androidVerison);
           } else {
-            print(map['3']['title']);
+            AndroidVerison androidVerison =
+                AndroidVerison(id: map['3']['id'], title: map['3']['title']);
+            androidListWithRandomKeys.add(androidVerison);
           }
         }
       }
     }
-
-    return androidListWithRandomKeys;
   }
 
+//TODO sending data based on the button click data set one or two
   @override
   void initState() {
     getAndroidVersions(StaticVariables.datasetOne);
@@ -97,25 +99,54 @@ class _HomeScreenState extends State<HomeScreen> {
           },
         ),
       ]),
-      body: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder(
-                future: getAndroidVersions(StaticVariables.datasetOne),
-                builder:
-                    (context, AsyncSnapshot<List<AndroidVerison>> snapshot) {
-                  return ListView.builder(
-                      itemCount: 8,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text('Ids'),
-                          subtitle: Text("SOme"),
-                        );
-                      });
-                }),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 20,
+                width: MediaQuery.of(context).size.width,
+                child: gridData(androidListWithRandomKeys),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 20,
+                width: MediaQuery.of(context).size.width,
+                child: gridData(androidList),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget gridData(var data) {
+    int countOfItem = 3;
+    if (data.length >= 3) {
+      setState(() {
+        countOfItem = 4;
+      });
+    }
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisSpacing: 50,
+          crossAxisCount: countOfItem,
+        ),
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return GridTile(
+              child: Text(
+            data[index].title,
+            style: TextStyle(color: Colors.black, fontSize: 14),
+          ));
+        });
   }
 }
